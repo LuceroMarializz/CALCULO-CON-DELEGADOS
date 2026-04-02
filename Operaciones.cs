f36a5713f4731c2f05ee0e8bba36691703045010
LuceroMarializz <ll2025-0305@uce.edu.do>
2026-04-02 17:51:43 -0400

    Add files via upload

---
+++ Operaciones.cs
@@ -0,0 +1,35 @@
+﻿using System;  
+
+namespace CALCULO_CON_DELEGADO
+{
+    public class PRESENTE_CLASE
+    {
+        public static int SUMAR(int A, int B)
+        {
+            return A + B;
+        }
+
+        public static int RESTAR(int A, int B)
+        {
+            return A - B;
+        }
+
+        public static int MULTIPLICAR(int A, int B)
+        {
+            return A * B;
+        }
+
+        public static int DIVIDIR(int A, int B)
+        {
+            if (B == 0)
+            {
+                Console.WriteLine("NO SE PUEDE DIVIDIR ENTRE CERO, LAMENTABLEMENTE");
+                return 0; 
+            }
+            else
+            {
+                return A / B;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace CALCULO_CON_DELEGADO
+{
+    class Program
+    {
+        // DELEGADO
+        public delegate int OperationMatematica(int A, int B);
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CALCULADORA CON DELEGADOS");
+
+            Console.Write("INGRESE EL PRIMER NÚMERO: ");
+            int N1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("INGRESE EL SEGUNDO NÚMERO: ");
+            int N2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nSELECCIONE UNA OPERACIÓN:");
+            Console.WriteLine("1. SUMAR");
+            Console.WriteLine("2. RESTAR");
+            Console.WriteLine("3. MULTIPLICAR");
+            Console.WriteLine("4. DIVIDIR");
+
+            Console.Write("SELECCION: ");
+            int SELECCION = Convert.ToInt32(Console.ReadLine());
+
+            OperationMatematica[] Operaciones = { PRESENTE_CLASE.SUMAR, PRESENTE_CLASE.RESTAR, PRESENTE_CLASE.MULTIPLICAR, PRESENTE_CLASE.DIVIDIR };
+
+            // EJECUTO MI DELEGADO
+            int PRODUCTO = Operaciones[SELECCION - 1](N1, N2);
+
+            Console.WriteLine($"\nPRODUCTO: {PRODUCTO}");
+
+            Console.ReadKey();
+        }
+    }
+}
